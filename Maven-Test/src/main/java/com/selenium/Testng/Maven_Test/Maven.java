a14267e2b69e337bebf62a36493ba7f800ebf8a2
ITsupport <ITsupport@RDT100.mlad.in>
2019-07-04 16:41:21 +0530

    fIRST COMMIT

---
+++ Maven-Test/src/main/java/com/selenium/Testng/Maven_Test/Maven.java
@@ -0,0 +1,17 @@
+package com.selenium.Testng.Maven_Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Maven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ITsupport\\Desktop\\New folder (2)\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://connect.maveric-systems.com/");
+
+	}
+
+}
