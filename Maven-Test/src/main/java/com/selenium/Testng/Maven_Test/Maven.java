8cfae1ce7704e58b7237fa82bf5686ec1a3d27e3
ITsupport <ITsupport@RDT100.mlad.in>
2019-07-04 17:08:55 +0530

    gggfhhsfd

--- Maven-Test/src/main/java/com/selenium/Testng/Maven_Test/Maven.java
+++ Maven-Test/src/main/java/com/selenium/Testng/Maven_Test/Maven.java
@@ -14,6 +14,8 @@ public class Maven {
 		driver.get("https://connect.maveric-systems.com/");
 		driver.get("https://connect.maveric-systems.com/");
 		driver.get("https://connect.maveric-systems.com/");
+		driver.get("https://connect.maveric-systems.com/");
+
 		
 		
 
